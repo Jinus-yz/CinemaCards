932fabaa963bb752d577a3b2ff997658c4f33202
Jinus <iamjinus@gmail.com>
2016-11-12 03:36:22 +0300

    Почистил using и исправил объявление св-ва name

--- CinemaCards/Classes/FilmCard.cs
+++ CinemaCards/Classes/FilmCard.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CinemaCards.Classes
 {
     class FilmCard
     {
-        String name;
+        public String name { get; private set; }
         public FilmCard(string iName)
         {
             name = iName;
